b510025224c57f52625f30582458d603886188b8
GYHUANG\user <l0958105106@gmail.com>
2021-04-07 17:51:33 +0800

    del nav，appbar router

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -9,27 +9,115 @@
   >
     <template v-slot:img="{ props }">
       <v-img
+        light
         v-bind="props"
         gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
       ></v-img>
     </template>
+
     <v-spacer></v-spacer>
 
-    <v-btn icon class="rotating">
-      <v-badge dot>
-        <v-icon>mdi-cart</v-icon>
-      </v-badge>
-    </v-btn>
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      offset-x
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon class="rotating" dark v-bind="attrs" v-on="on">
+          <v-badge dot>
+            <v-icon>mdi-cart</v-icon>
+          </v-badge>
+        </v-btn>
+      </template>
 
+      <v-card>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>John Leider</v-list-item-title>
+              <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch v-model="message" color="purple"></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable messages</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch v-model="hints" color="purple"></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable hints</v-list-item-title>
+          </v-list-item>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text @click="menu = false">
+            Cancel
+          </v-btn>
+          <v-btn color="primary" text @click="menu = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
     <template v-slot:extension>
       <v-tabs align-with-title>
-        <v-tab>Tab 1</v-tab>
-        <v-tab>Tab 2</v-tab>
-        <v-tab>Tab 3</v-tab>
+        <v-tab
+          v-for="item in items"
+          :key="item.title"
+          link
+          @click="routePush(item.route)"
+          >{{ item.title }}
+        </v-tab>
       </v-tabs>
     </template>
   </v-app-bar>
 </template>
+
+<script>
+import router from "@/router/index.js";
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Home", icon: "mdi-view-dashboard", route: "/home" },
+        { title: "About", icon: "mdi-image", route: "/about" },
+      ],
+      right: null,
+      fav: true,
+      menu: false,
+      message: false,
+      hints: true,
+    };
+  },
+  methods: {
+    routePush: function(path) {
+      router.push(path,()=>{});
+    },
+  },
+};
+</script>
 
 <style>
 @-webkit-keyframes rotating /* Safari and Chrome */ {
